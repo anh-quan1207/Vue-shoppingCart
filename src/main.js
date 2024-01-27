ba2aeaae7565d90fcac20fe42879b0f7b71f8d2f
quan.phamanh <quan.phamanh@vti.com.vn>
2024-01-27 13:49:57 +0700

    first

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/jquery/dist/jquery.js"
+import "../node_modules/popper.js/dist/popper.js"
+import "../node_modules/bootstrap/dist/js/bootstrap.js"
+
+createApp(App).mount('#app')
